6cd52a572c2a9000f607cdeef8e9666308f0b538
mtrifilo <matt.trifilo@gmail.com>
2017-07-28 12:06:25 -0700

    add Date_Ex1

---
+++ src/Date_Ex1.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+class Date_Ex1 {
+    public static void main(String args[]) {
+        // Instantiate a Date Object by invoking its constructor
+        Date objDate = new Date();
+
+        // Display the Date and Time using toString()
+        System.out.println(objDate.toString());
+    }
+}
